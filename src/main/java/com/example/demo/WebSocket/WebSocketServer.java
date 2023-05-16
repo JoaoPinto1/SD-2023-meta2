3e2b0c1211b0f846d00dc3955c3590fdf79af263
Andre Moreira <andre-f-moreira@hotmail.com>
2023-05-16 02:37:18 +0100

    commit

--- src/main/java/com/example/demo/WebSocket/WebSocketServer.java
+++ src/main/java/com/example/demo/WebSocket/WebSocketServer.java
@@ -9,14 +9,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableScheduling
 public class WebSocketServer implements WebSocketMessageBrokerConfigurer {
+
+
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/my-websocket").withSockJS();
+        registry.addEndpoint("/websocket").withSockJS();
     }
+
 }
